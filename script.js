5f6751dcf70f43dd857642f8f9f9db924c9169ca
rkhoury11 <rkhoury11@icloud.com>
2024-03-10 21:11:39 -0400

    finished 100%

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleSidebar() {
     // Adjust the position of sidebar icon
     const navToggle = document.querySelector('.nav-toggle');
     if (sidebar.classList.contains('active')) {
-        navToggle.style.left = '270px'; // Adjust based on sidebar width
+        navToggle.style.left = '270px';
     } else {
-        navToggle.style.left = '20px'; // Adjust as needed
+        navToggle.style.left = '20px';
     }
 }
 
